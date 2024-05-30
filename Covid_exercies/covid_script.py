0ddefb5ac4ffbd2a5b4bf26c265843c752267091
l-zaehle <lzaehle@unibz.it>
2024-05-30 21:47:50 +0200

    Update covid_script.py
    Working dictionary with regions and all province geometries

--- Covid_exercies/covid_script.py
+++ Covid_exercies/covid_script.py
@@ -37,18 +37,28 @@ for feature in regionsFeatures:
 for item in regions:
     for feature in regionsFeatures:
         region = feature.attributes[regionIndex]
+        if region not in regionsDict:
+            regionsDict[region] = []
         if item == region:
-            regionsDict[region]: feature.geometry
+            regionsDict[region].append(feature.geometry)
 
+print(regionsDict)
+"""
 fields = {
     "name": "String"
 }
 
 regionsLayer = HVectorLayer.new("regions", "Polygon", "EPSG:4326", fields)
 
-for region, geo in regionsDict.items():
+for region, PGeometries in regionsDict.items():
+    count = 0
+    geo = PGeometries[-1]
+    for i in range(len(PGeometries)):
+        geo = geo.merge(PGeometries[count])
+        count =+1
+        
     regionsLayer.add_feature(geo, [region])
 
 
 HMap.add_layer(regionsLayer)
-
+"""
