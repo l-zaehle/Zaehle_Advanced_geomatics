ebf84e8792401e5aab2f798570d5e16c690b02b9
l-zaehle <lzaehle@unibz.it>
2024-06-12 21:00:31 +0200

    Everyone is on board
    lets get started
    
    Co-Authored-By: aschtetter <170632492+aschtetter@users.noreply.github.com>
    Co-Authored-By: paulasch96 <171152382+paulasch96@users.noreply.github.com>
    Co-Authored-By: nmolodow <170632396+nmolodow@users.noreply.github.com>

--- Covid_exercies/covid_script.py
+++ Covid_exercies/covid_script.py
@@ -24,35 +24,38 @@ provincesLayer.subset_filter("admin = 'Italy'") #just show those features that a
 regionIndex = provincesLayer.field_index("region") 
 provinceIndex = provincesLayer.field_index("name")
 
-regionsFeatures = provincesLayer.features()
+provinceFeatures = provincesLayer.features()
 
 #create a list with all the regions of italy
 regions = []
-for feature in regionsFeatures:
+for feature in provinceFeatures:
     region = feature.attributes[regionIndex]
     if region not in regions:
         regions.append(region)
+##print(regions)
 
-#create a dictionary with the region as key and geometries as values
+#create a dictionary with the region as key and every single Province geometries as values
 #Loop over the regions-list and check, if region is not yet in the dict, In this case a key (regionName) is set and the values are an empty list
 #then all geometries for each key are being added to the values-list
 regionsDict = {}
 for item in regions:
-    for feature in regionsFeatures:
+    for feature in provinceFeatures:
         region = feature.attributes[regionIndex]
         if region not in regionsDict:
             regionsDict[region] = []
         if item == region:
             regionsDict[region].append(feature.geometry)
 
-
+##print(regionsDict)
 # creation of the regionsLayer
 fields = {
     "name": "String"
 }
 
-regionsLayer = HVectorLayer.new("regions", "Polygon", "EPSG:4326", fields)
+regionsLayer = HVectorLayer.new("regions", "MultiPolygon", "EPSG:4326", fields)
 
+#for loop accessing all elements in regionsDict
+#region = key, PGeometries = values
 for region, PGeometries in regionsDict.items():
     count = 0
     geo = PGeometries[-1]
